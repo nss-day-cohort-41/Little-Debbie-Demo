f69659981100895e8ce8d98c0281dfc58a6cf6cb
Brenda Long <brenda@studio6thave.com>
2020-06-18 10:37:26 -0500

    added post method for adding a sweet

--- js/dbCalls.js
+++ js/dbCalls.js
@@ -4,6 +4,16 @@ const API = {
 	getAllSweets: () => {
 		return fetch(`${url}/sweet`)
 		.then(response => response.json());
+	},
+	addASweet: (sweetObj) => {
+		return fetch(`${url}/sweet`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(sweetObj)
+		}).then(response => response.json());
+
 	}
 }
 
--- js/main.js
+++ js/main.js
@@ -10,11 +10,27 @@ const newSweet2 = makeSweet("Football Brownies", 6, "Looks like a football", 4, 
 console.log("this is the new sweet", newSweet1);
 console.log("this is the second sweet", newSweet2);
 
+///////////////////////////////////
+
+// go get all the data
 const allSweets = () => {
+	// API is an object that has multiple methods for interacting with the DB
 	API.getAllSweets()
 	.then((sweets) => {
+		// API.getAllSweets returns data, in this case an array
 		console.log(sweets);
 	})
 }
 
-allSweets();
+//invoke the allSweets function
+// allSweets();
+
+// using the makeSweet factory function, make a sweet object
+const newSweet3 = makeSweet("Christmas In December 31", 5, "White Christmas Tree with Santa on the Package", 1, 2, 3);
+
+//invoke the addASweet method and pass it the newSweet3 object
+API.addASweet(newSweet3)
+.then(() => {
+	//Once the new sweet has been added to the DB, go get all the data again.
+	allSweets();
+});
